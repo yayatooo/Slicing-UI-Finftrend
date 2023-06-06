67779e8e582f7826e15fa98d2ffe7426f88dce0c
Rahmat Hidayat <yayatbx@gmail.com>
2023-06-07 01:17:28 +0800

    fixed bug deploy

--- src/app/components/LongtextSection.js
+++ src/app/components/LongtextSection.js
@@ -4,8 +4,8 @@ const LongtextSection = () => {
   return (
     <div className="w-10/12 py-16 mx-auto">
       <h1 className="text-7xl font-effra-heavy py-16 mobile:text-4xl">
-        Findtrend helps you to increase your productivity and reduce your
-        computer's memory load,
+        {`Findtrend helps you to increase your productivity and reduce your
+        computer's memory load,`}
         <span className="text-[#ADADAD]">
           an application that can fulfill your daily browsing needs.
         </span>
